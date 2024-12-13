0b531275c1874747f3f414806c4e25ba88536e12
0skar <0skar01@hotmail.se>
2024-12-13 19:50:27 +0100

    Create Country class and SQL table

---
+++ src/main/java/se/iths/Country.java
@@ -0,0 +1,42 @@
+package se.iths;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "country", schema = "db_proj")
+
+
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
