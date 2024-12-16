48c76fb2d689d169fd2ceaf120372f1535ac77e5
EvaHottinen <eva.hottinen@iths.se>
2024-12-16 10:48:08 +0100

    create tabel student

--- schema.sql
+++ schema.sql
@@ -2,6 +2,46 @@ use db_proj;
 
 CREATE TABLE educator
 (
-    id   INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id   INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 )
+
+CREATE TABLE student (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    country VARCHAR(255) FOREIGN KEY (country) REFERENCES country(id),
+    school VARCHAR(255) FOREIGN KEY (school) REFERENCES school(id),
+    course VARCHAR(255) FOREIGN KEY (course) REFERENCES course(id)
+);
+
+INSERT INTO student(name, country, school, course)
+VALUES ('Harry Potter', 'England', '', ''),
+       ('Ron Weasley', 'England', '', ''),
+       ('Hermione Granger', 'England', '', ''),
+       ('Neville Longbottom', 'England', '', ''),
+       ('Luna Lovegood', 'England', '', ''),
+       ('Fred Weasley', 'England', '', ''),
+       ('George Weasley', 'England', '', ''),
+       ('Ginny Weasley', 'England', '', ''),
+       ('Cho Chang', 'England', '', ''),
+
+       ('Nicolas Flamel', 'France', '', ''),
+       ('Aurélie Dumont', 'France', '', ''),
+       ('Celine Castillon', 'France', '', ''),
+       ('Fleur Delacour', 'France', '', ''),
+       ('Gabrielle Delacour', 'France', '', ''),
+       ('Élise Martin', 'France', '', ''),
+       ('Sylvie Garnier', 'France', '', ''),
+       ('Perenelle Flamel', 'France', '', ''),
+       ('Luc Millefeuille', 'France', '', ''),
+
+       ('Malin Matsdotter', 'Sweden', '', ''),
+       ('Gertrud Svensdotter', 'Sweden', '', ''),
+       ('Märit Jonsdotter', 'Sweden', '', ''),
+       ('Elisabetta Näs', 'Sweden', '', ''),
+       ('Margareta Nilsdotter', 'Sweden', '', ''),
+       ('Anna Olsdotter', 'Sweden', '', ''),
+       ('Anna Månsdotter', 'Sweden', '', ''),
+       ('Britta Sippel', 'Sweden', '', ''),
+       ('Anna Sippel', 'Sweden', '', '');
+
