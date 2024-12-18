c12a4299f335aa8c74b8019ec820d968ebd9ffd7
Benjamin Olsson Bjerke <benjibus04@gmail.com>
2024-12-18 13:48:36 +0100

    Fixed schema

--- schema.sql
+++ schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE educator
     id   INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 )
+
+CREATE TABLE school
+(
+    id VARCHAR NOT NULL PRIMARY KEY,
+    namne VARCHAR(255)
+    contry_id BIGINT NOT NULL,
+    FOREIGN KEY (contry_id) REFERENCES country(id) ON DELETE CASCADE
+)
